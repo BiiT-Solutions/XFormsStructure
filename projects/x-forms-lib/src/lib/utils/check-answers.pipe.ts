460c04a343c813600b319c6f402d94a68ad8514c
Jesus Pastor <jnavalon@biit-solutions.com>
2024-01-20 11:15:45 +0100

    fixed submit completed form  detector

--- projects/x-forms-lib/src/lib/utils/check-answers.pipe.ts
+++ projects/x-forms-lib/src/lib/utils/check-answers.pipe.ts
@@ -4,20 +4,14 @@ import {FormItem} from "../models/form-item";
 import {CategoryComponent} from "../shared/category/category.component";
 
 @Pipe({
-  name: 'checkAnswers'
+  name: 'checkAnswers', pure: false
 })
 export class CheckAnswersPipe implements PipeTransform {
 
   transform(form: Form): boolean {
-    debugger
     if (!form) {
       return true;
     }
     return form.children.filter(category => category.display).every(CategoryComponent.isCompleted);
-  }
-
-  private checkAnswers(item: FormItem): void {
-
-
   }
 }
