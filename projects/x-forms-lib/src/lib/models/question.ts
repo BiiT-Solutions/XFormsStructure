802f0b9971835eb936978786ee909a8cd8e52d12
«anonymous» <usuari@domini.com>
2024-02-24 14:24:14 +0100

    added default values

--- projects/x-forms-lib/src/lib/models/question.ts
+++ projects/x-forms-lib/src/lib/models/question.ts
@@ -2,6 +2,8 @@ import {AnswerType} from "./answer-type";
 import {VariableType} from "./variable-type";
 import {VariableFormat} from "./variable-format";
 import {Directional} from "./directional";
+import {FormItem} from "./form-item";
+import {Answer} from "./answer";
 
 export class Question<T> extends Directional {
   answerType: AnswerType;
@@ -25,10 +27,31 @@ export class Question<T> extends Directional {
     to.horizontal = from.horizontal;
     to.description = from.description;
     to.response = from.response;
+    Question.setDefaultValues(from, to);
   }
   public static override clone<T>(from: Question<T>): Question<T> {
     const to: Question<T> = new Question<T>();
     Question.copy(from, to);
     return to;
+  }
+
+  // This method is a workaround to merge all the default values
+  private static setDefaultValues<T>(from: any, to: Question<T>): void {
+    if (from['defaultValueString']) {
+      to.response = from['defaultValueString'];
+      to.valid = true;
+    }
+    if (from['defaultValueAnswer']) {
+      const answer: FormItem = to.children.find(child => child.name === from['defaultValueAnswer']);
+      if (answer && answer instanceof Answer) {
+        answer.selected = true;
+        to.valid = true;
+      }
+      to.response = answer as T;
+    }
+    if (from['defaultValueTime']) {
+      to.response = new Date(from['defaultValueTime']) as T;
+      to.valid = true;
+    }
   }
 }
