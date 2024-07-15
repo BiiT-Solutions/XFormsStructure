9d6e1344e31142cf6a6ef52c0f85859f43fec8c7
Jesus Pastor <jnavalon@biit-solutions.com>
2024-07-15 17:58:00 +0200

    added label to sliders

--- projects/x-forms-lib/src/lib/models/group.ts
+++ projects/x-forms-lib/src/lib/models/group.ts
@@ -6,12 +6,14 @@ export class Group extends FormItem {
   isTable: boolean;
   numberOfColumns: number;
   duplicated: boolean = false;
+  totalAnswersValue: number;
 
   public static override copy(from: Group, to: Group): void {
     super.copy(from, to);
     to.repeatable = from.repeatable;
     to.isTable = from.isTable;
     to.numberOfColumns = from.numberOfColumns;
+    to.totalAnswersValue = from.totalAnswersValue;
   }
   public static override clone(from: Group): Group {
     const to: Group = new Group();
