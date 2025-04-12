d313f749f4798a1eb292b112e0af5997a02b1817
Jesus Navalon <jnavalon@biit-solutions.com>
2025-04-12 17:27:55 +0200

    added clone and copy to formResult

--- projects/x-forms-lib/src/lib/models/form/form-item.ts
+++ projects/x-forms-lib/src/lib/models/form/form-item.ts
@@ -6,8 +6,29 @@ export class FormItem {
   name: string;
   label: string;
   labelTranslations: {[key: string]: string};
+
   constructor(clazz: string) {
     this.class = clazz;
    }
+
+  public static clone(from: FormItem): FormItem {
+    const to: FormItem = new FormItem(from.class);
+    this.copy(from, to);
+    return to;
+  }
+
+  public static copy(from: FormItem, to: FormItem): void {
+    to.comparationId = from.comparationId;
+    to.creationTime = from.creationTime;
+    to.updateTime = from.updateTime;
+    to.name = from.name;
+    to.label = from.label;
+    to.labelTranslations = {};
+    if (from.labelTranslations) {
+      Object.keys(from.labelTranslations).forEach((key: string) => {
+        to.labelTranslations[key] = from.labelTranslations[key];
+      });
+    }
+  }
 
 }
--- projects/x-forms-lib/src/lib/models/form/form-result.ts
+++ projects/x-forms-lib/src/lib/models/form/form-result.ts
@@ -10,4 +10,20 @@ export class FormResult extends FormItem {
   constructor() {
     super('com.biit.form.result.FormResult');
   }
+
+  public static override clone(from: FormResult): FormResult {
+    const to: FormResult = new FormResult();
+    this.copy(from, to);
+    return to;
+  }
+
+  public static override copy(from: FormResult, to: FormResult): void {
+    super.copy(from, to);
+    to.submittedAt = from.submittedAt;
+    to.updatedBy = from.updatedBy;
+    to.version = from.version;
+    to.organizationId = from.organizationId;
+    to.children = from.children ? from.children.map(FormItem.clone) : [];
+  }
+
 }
