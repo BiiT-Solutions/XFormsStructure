5728907f3dd053d33546e2e3dc93cf475a93a4e4
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-05-19 10:46:34 +0200

    Adding translations to FR and DE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,16 @@ import {registerLocaleData} from "@angular/common";
 import localeEn from '@angular/common/locales/en';
 import localeEs from '@angular/common/locales/es';
 import localeNL from '@angular/common/locales/nl';
+import localeFr from '@angular/common/locales/fr';
+import localeDe from '@angular/common/locales/de';
 import {TRANSLOCO_CONFIG, TRANSLOCO_LOADER, translocoConfig} from "@ngneat/transloco";
 import {TranslocoHttpLoader} from "./transloco-root.module";
 
 registerLocaleData(localeEn, 'en')
 registerLocaleData(localeEs, 'es');
 registerLocaleData(localeNL, 'nl');
+registerLocaleData(localeFr, 'fr');
+registerLocaleData(localeDe, 'de');
 
 @NgModule({
   declarations: [
